e2b1f809ce288dc64cbcc79be2140b4c88572c0d
Joey Chrysanthopoulos <joeyachrys@outlook.com>
2022-05-26 13:36:57 -0500

    set up action and slice for email api states

--- frontend/src/partials/EmailCard.jsx
+++ frontend/src/partials/EmailCard.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import EmailForm from "../components/EmailForm";
-import Button from "../components/Button";
 
 const EmailCard = () => {
   return (
-    <div class="w-[20rem] bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 p-6">
+    <div className="w-[20rem] bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 p-6">
       <h1 className="block mb-6 text-lg font-medium text-gray-900 dark:text-gray-300">
         Mailing List Sign Up
       </h1>
       <EmailForm />
-      <Button />
     </div>
   );
 };
